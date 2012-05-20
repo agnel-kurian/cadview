3dc1a0e1826308004708b81cec6bfc1a7e9def44
Agnel C J Kurian <agnel.kurian@gmail.com>
2012-05-20 18:19:06 +0530

    homogeneous = FALSE when calling gtk_vbox_new; otherwise, menu bar and client control will be equal height

--- cadview/main.cpp
+++ cadview/main.cpp
@@ -1,6 +1,11 @@
 #include <gtk/gtk.h>
 #include "slate.h"
 
+void menu_file_quit(GtkMenuItem *menuitem, gpointer user_data){
+  fprintf(stderr, "quitting\n");
+  gtk_main_quit();
+}
+
 int main(int argc, char *argv[]){
   GtkWidget *window;
   GtkWidget *slate;
@@ -13,10 +18,22 @@ int main(int argc, char *argv[]){
 
   slate = gtk_slate_new();
 
-  vbox = gtk_vbox_new(TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(vbox), slate, TRUE, TRUE, 3);
-
+  vbox = gtk_vbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  GtkWidget *menubar = gtk_menu_bar_new();
+  GtkWidget *filemenu = gtk_menu_new();
+  GtkWidget *file = gtk_menu_item_new_with_mnemonic("_File");
+  GtkWidget *quit = gtk_menu_item_new_with_mnemonic("_Quit");
+  g_signal_connect(G_OBJECT(quit), "activate",
+    G_CALLBACK(menu_file_quit), NULL);
+  gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quit);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
+
+  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
+
+  gtk_box_pack_start(GTK_BOX(vbox), slate, TRUE, TRUE, 0);
 
   g_signal_connect(G_OBJECT(window), "destroy",
     G_CALLBACK(gtk_main_quit), NULL);
